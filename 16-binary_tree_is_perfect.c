60110c172eb59917dd6ab4346c04a37005c8f7fb
MiguelCF06 <miguel.cipamocha@gmail.com>
2020-07-21 16:50:24 +0000

    [Updated]Fixed the height function

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,5 +1,6 @@
 #include "binary_trees.h"
 
+
 /**
  *binary_tree_height - measures the height of a binary tree
  *@tree: is a pointer to the root node of the tree
@@ -13,7 +14,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		return (0);
 
 	if (tree->left == NULL && tree->right == NULL)
-		return (1);
+		return (0);
 
 	left = binary_tree_height(tree->left);
 	right = binary_tree_height(tree->right);
