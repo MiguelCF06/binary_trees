0ec4c500ac02f45a2f528d7a30b9d8d1b1c0427c
MiguelCF06 <miguel.cipamocha@gmail.com>
2020-07-21 15:32:18 +0000

    [Updated]Fixed the second if

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -13,12 +13,12 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		return (0);
 
 	if (tree->left == NULL && tree->right == NULL)
-		return (0);
+		return (1);
 
 	left = binary_tree_height(tree->left);
 	right = binary_tree_height(tree->right);
 
 	if (left >= right)
 		return (left + 1);
-	return (right);
+	return (right + 1);
 }
